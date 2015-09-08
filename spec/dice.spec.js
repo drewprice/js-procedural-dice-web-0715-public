519413ebf2b2f253181f8837f61c44c4abd849ed
Drew Price <drewprice@users.noreply.github.com>
2015-09-08 17:40:01 -0400

    Add solution

--- spec/dice.spec.js
+++ spec/dice.spec.js
@@ -1,18 +1,19 @@
 'use strict';
+// var roll = require("../lib/dice");
 
 describe('dice', function() {
 
   describe("Dice#roll", function(){
     it("should return a number", function() {
       expect(typeof roll()).toBe("number");
-    });      
+    });
 
     it("should return a number greater than 0", function() {
       expect(roll()).toBeGreaterThan(0);
-    });  
+    });
 
     it("should return a number less than 7", function() {
       expect(roll()).toBeLessThan(7);
-    });  
+    });
   });
 });
